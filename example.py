22d3524c7ba5d60e6b6c02dccab90bd3568b0690
Andres Unigarro <ap.unigarro@gmail.com>
2026-04-09 19:57:18 +0200

    example.py file corrected

--- example.py
+++ example.py
@@ -4,13 +4,16 @@ from fatbands import FatbandsPlotter
 
 ############# example of usage
 # Initialize with the example file
-plotter = FatbandsPlotter("path_to_file_FATBANDS.nc")
+viewer = FatbandsPlotter("PATH_TO_FATBANDS.nc")
 
 # Call plot function for L (symbol) and LM (symbol_m) resolved fatbands for te atom type specified.
-viewer.plot_fatbands_symbol(band_list=list(range(150,250)), e0=2.77561,
-                                l=1,ylims=[-2,2], symbol=['Si','C'], symbol_m='Pb'
-                                xticks=['G','K','M','K'], 
-                                xval_ticks=[0,30,60,90],
+viewer.plot_fatbands_symbol(band_list=list(range(150,250)), #### bands to plot
+                                e0=2.77561, ### Fermi level
+                                l=1,ylims=[-2,2], 
+                                symbol=['Si','C'], ### atoms set used for the orbital projection
+                                symbol_m='Pb', ### atoms set used for the m-resolved orbital projection
+                                xticks=['G','K','M','K'],  ###  ticks for the x-axis
+                                xval_ticks=[0,30,60,90], ### where the ticks are located
                                 save_path='./plot.png')
 
 
@@ -19,8 +22,12 @@ Pb=atom1=[0,1,2]
 Gr=atom1=[3,4,5,6,7,8,9,10]
 SiC=list(range(11,50))
 
-viewer.plot_fatbands_atomsets(band_list=list(range(150,250)), e0=2.77561,
-                                l=1,ylims=[-2,2], atom_set=[0,1,2], atom_set_m=[[3,4,5,6,7,8,9,10],list(range(11,50))]
-                                xticks=['G','K','M','K'], 
-                                xval_ticks=[0,30,60,90],
-                                save_path='./plot.png')
+viewer.plot_fatbands_atomsets(band_list=list(range(150,250)),  #### bands to plot
+                                e0=2.77561, #### Fermi level
+                                l=1, ### angular momentum to make the orbital projection 
+                                ylims=[-2,2], 
+                                atom_set=[0,1,2], ### atoms set used for the orbital projection
+                                atom_set_m=[[3,4,5,6,7,8,9,10],list(range(11,50))], ### atoms set used for the m-resolved orbital projection
+                                xticks=['G','K','M','K'], ###  ticks for the x-axis
+                                xval_ticks=[0,30,60,90], ### where the ticks are located
+                                save_path='./plot.png') 
